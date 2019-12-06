c77e188b9e2bc65c0613a7cff627905c620e4d35
JoalisonMatheuss <44174090+JoalisonMatheuss@users.noreply.github.com>
2019-12-06 11:27:29 -0300

    lanchonete

---
+++ Lanchonete.sql
@@ -0,0 +1,28 @@
+﻿CREATE TABLE Cliente(
+CodCliente integer PRIMARY KEY,
+Nome varchar(50)
+);
+
+CREATE TABLE Comanda(  --Vulgo Venda
+ NumComanda integer PRIMARY KEY,
+ ValorTotalVenda numeric(11,2),
+ CodCliente integer REFERENCES Cliente(CodCliente)
+);
+
+CREATE TABLE Produto(
+CodProduto integer PRIMARY KEY,
+Custo numeric(11,2),
+Descricao varchar(150),
+Lucro numeric(11,2),
+Preco numeric(11,2),
+Nome varchar(50)
+);
+
+
+CREATE TABLE Item_Venda(
+CodProduto integer REFERENCES Produto(CodProduto),
+NumComanda integer REFERENCES Comanda(NumComanda),
+ValorUnitario numeric(11,2),
+Qtd integer
+);
+
